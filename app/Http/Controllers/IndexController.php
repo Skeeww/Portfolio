e5880232a04a384d109a29c2454941190c9f282a
Noan <noan.perrot@gmail.com>
2022-05-25 20:43:25 +0200

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index() {
-        $nb_gitlab_repositories = count(json_decode(file_get_contents("https://gitlab.com/api/v4/projects?private_token=".env('GITLAB_TOKEN')."&owned=true")));
+        try {
+            $nb_gitlab_repositories = count(json_decode(file_get_contents("https://gitlab.com/api/v4/projects?private_token=".env('GITLAB_TOKEN')."&owned=true")));
+        } catch(Exception $e) {
+            $nb_gitlab_repositories = 404;
+        }
         return view('index', ['nb_repo' => $nb_gitlab_repositories]);
     }
 }
